e2af88675a08421dda2a2e3d0c65031f5389ac58
Jason Hughes <20440805+JasonHughes94@users.noreply.github.com>
2019-10-11 13:56:46 +0100

    Sending messages and adding a header

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './Components/Header';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TextInput,
+  KeyboardAvoidingView,
+  SafeAreaView,
+  TouchableOpacity,
+  Image
+} from 'react-native';
 import { send, subscribe } from './chatServer';
 
 const NAME = 'Jason';
 const CHANNEL = 'hackconf';
+const AVATAR =
+  'https://media.licdn.com/dms/image/C5603AQEkF_gKVCL4LA/profile-displayphoto-shrink_800_800/0?e=1576108800&v=beta&t=NNeEbKGTR5BoembdD0b9U7gl70ssnbkorpklj7ZWhME';
 
 const renderItem = ({ item }) => {
   return (
     <View style={styles.row}>
-      <Text style={styles.sender}>{item.sender}</Text>
-      <Text style={styles.message}>{item.message}</Text>
+      <Image style={styles.avatar} source={{ uri: item.avatar }} />
+      <View style={styles.rowText}>
+        <Text style={styles.sender}>{item.sender}</Text>
+        <Text style={styles.message}>{item.message}</Text>
+      </View>
     </View>
   );
 };
 
 export default function App() {
   const [messages, setMessages] = React.useState([]);
+  const [text, setText] = React.useState('');
 
   React.useEffect(() => {
     subscribe(CHANNEL, messages => {
@@ -23,10 +40,36 @@ export default function App() {
     });
   }, []);
 
+  const sendMessage = React.useCallback(async () => {
+    await send({
+      channel: CHANNEL,
+      sender: NAME,
+      message: text,
+      avatar: AVATAR
+    });
+
+    setText('');
+  }, [text]);
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <Header title={CHANNEL} />
       <FlatList data={messages} renderItem={renderItem} inverted />
-    </View>
+      <KeyboardAvoidingView behavior="padding">
+        <View style={styles.footer}>
+          <TextInput
+            value={text}
+            onChangeText={setText}
+            style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder="Type something nice"
+          />
+          <TouchableOpacity onPress={sendMessage}>
+            <Text style={styles.send}>Send</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
 
@@ -36,10 +79,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#e3e3e3'
   },
   row: {
+    flexDirection: 'row',
     padding: 20,
     borderBottomWidth: 1,
     borderBottomColor: '#e3e3e3',
-    backgroundColor: '#eee'
+    backgroundColor: '#eee',
+    borderRadius: 12,
+    marginBottom: 12,
+    marginLeft: 12,
+    marginRight: 12
   },
   message: {
     fontSize: 18
@@ -47,5 +95,31 @@ const styles = StyleSheet.create({
   sender: {
     fontWeight: 'bold',
     paddingRight: 10
+  },
+  footer: {
+    flexDirection: 'row',
+    backgroundColor: '#e3e3e3'
+  },
+  input: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    fontSize: 18,
+    flex: 1
+  },
+  send: {
+    alignSelf: 'center',
+    color: 'teal',
+    fontSize: 16,
+    fontWeight: 'bold',
+    padding: 20
+  },
+  avatar: {
+    borderRadius: 20,
+    width: 40,
+    height: 40,
+    marginRight: 10
+  },
+  rowText: {
+    flex: 1
   }
 });
---
+++ Components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default props => (
+  <View style={styles.header}>
+    <Text style={styles.title}>#{props.title}</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    backgroundColor: 'teal',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: 10
+  },
+  title: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 24
+  }
+});
