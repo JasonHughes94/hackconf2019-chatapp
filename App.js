f8703149c2e04dafec633f3037e07f94549d66af
Jason Hughes <20440805+JasonHughes94@users.noreply.github.com>
2019-10-11 13:25:58 +0100

    Pulling messages and rendering them from the chat server

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { send, subscribe } from './chatServer';
+
+const NAME = 'Jason';
+const CHANNEL = 'hackconf';
+
+const renderItem = ({ item }) => {
+  return (
+    <View style={styles.row}>
+      <Text style={styles.sender}>{item.sender}</Text>
+      <Text style={styles.message}>{item.message}</Text>
+    </View>
+  );
+};
 
 export default function App() {
+  const [messages, setMessages] = React.useState([]);
+
+  React.useEffect(() => {
+    subscribe(CHANNEL, messages => {
+      setMessages(messages);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList data={messages} renderItem={renderItem} inverted />
     </View>
   );
 }
@@ -12,8 +33,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e3e3e3'
   },
+  row: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#e3e3e3',
+    backgroundColor: '#eee'
+  },
+  message: {
+    fontSize: 18
+  },
+  sender: {
+    fontWeight: 'bold',
+    paddingRight: 10
+  }
 });
